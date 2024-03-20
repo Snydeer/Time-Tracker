05e72c528a64970916cf896835c266f5e4398ead
Rasmey Oung <158348034+rasmey129@users.noreply.github.com>
2024-03-20 13:25:35 -0400

    Frontend stuff

---
+++ app/js/script.js
@@ -0,0 +1,30 @@
+
+//main dashboard function buttons 
+
+function navigateToTimePunch() {
+    window.location.href = "/timePunch.html";
+}
+
+function navigateToTimeSheet() {
+    window.location.href = "/timeSheet.html";
+}
+
+function navigateToSchedule() {
+    window.location.href = "/Schedule.html";
+}
+
+function navigateToAnnouncement() {
+    window.location.href = "/announcements.html";
+}
+
+function navigateToPayPeriod() {
+    window.location.href = "/payPeriod.html";
+}
+
+function navigateToSettings() {
+    window.location.href = "/Settings.html";
+}
+
+function returnBut() {
+    window.location.href = "/main.html";
+}
