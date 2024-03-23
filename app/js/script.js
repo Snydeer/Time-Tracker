680cbcdacd04f120cc252395bb0a149bcb0c52d8
Nelson Ha <44574888+NelsonHa191@users.noreply.github.com>
2024-03-22 20:52:38 -0400

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,55 @@
+function navigateToLogin() {
+    window.location.href = "loginpicker.php";
+}
+
+function navigateToEmployeeLogin() {
+    window.location.href = "loginemployee.php";
+}
+
+function navigateToDashboard() {
+    window.location.href = "dashboard.php";
+}
+
+function navigateToHome() {
+    window.location.href = "index.php";
+}
+
+function navigateToLogout() {
+    window.location.href = "includes/logout.inc.php";
+}
+
+function navigateToManagerLogin() {
+    window.location.href = "login.php";
+}
+
+function navigateToProfile() {
+    window.location.href = "profile.php";
+}
+
+function navigateToTimePunch() {
+    window.location.href = "timetracker.php";
+}
+
+function navigateToTimeSheet() {
+    window.location.href = "timeview.php";
+}
+
+function navigateToSchedule() {
+    window.location.href = "/Schedule.html";
+}
+
+function navigateToAnnouncement() {
+    window.location.href = "/announcements.html";
+}
+
+function navigateToPayPeriod() {
+    window.location.href = "/payPeriod.html";
+}
+
+function navigateToSettings() {
+    window.location.href = "/Settings.html";
+}
+
+function returnBut() {
+    window.location.href = "/main.html";
+}
