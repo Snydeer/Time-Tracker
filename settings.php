9c828feb011965971e0c54684fd9d6a7b05bbd15
Avery Snyder <Asnyder884@gmail.com>
2024-03-26 15:33:36 -0400

    Added a bare bones settings page

---
+++ settings.php
@@ -0,0 +1,59 @@
+<?php
+    include_once 'header.php';
+?>
+
+<html>
+    <body>
+        <h1> Settings</h1>
+        <div class="buttons">
+
+        <?php
+                if (isset($_SESSION["useruid"])) {
+                    echo '
+                        <button class="main" onclick="navigateToCreateEmployee()">
+                            <span>Create</span>
+                        </button>
+                    ';
+                    echo '
+                        <button class="main" onclick="navigateToEmployeeTimesheet()">
+                            <span>Employee Timesheet</span>
+                        </button>
+                    ';
+                } elseif (isset($_SESSION["employeeuid"])) {
+                    echo '
+                        <button class="main" onclick="navigateToTimePunch()">
+                            <span>Time Punch</span>
+                         </button>
+                    ';
+                    echo '
+                        <button class="main" onclick="navigateToTimeSheet()">
+                            <span>Time Sheet</span>
+                         </button>
+                    ';
+                } else {
+                    header("location: ../index.php");
+                }
+            ?>
+        
+            <button class="main" onclick="navigateToSchedule()">
+                <span>Schedule</span>
+            </button>
+        
+            <button class="main" onclick="navigateToAnnouncement()">
+                <span>Announcements</span>
+              </button>
+
+            <button class="main" onclick="navigateToPayPeriod()">
+                <span>Pay Period</span>
+            </button>
+            
+            <button class="main" onclick="navigateToSettings()">
+                <span>Settings</span>
+            </button>
+            
+        
+
+        
+
+    </body>
+</html>
