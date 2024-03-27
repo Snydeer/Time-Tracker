cde9d1021431e5ba686d30e8e2ff4853b378e184
Snydeer <50647767+Snydeer@users.noreply.github.com>
2024-03-26 21:22:03 -0400

    Added settings page and fixed the dashboard buttons

--- settings.php
+++ settings.php
@@ -5,51 +5,14 @@
 <html>
     <body>
         <h1> Settings</h1>
-        <div class="buttons">
-
-        <?php
-                if (isset($_SESSION["useruid"])) {
-                    echo '
-                        <button class="main" onclick="navigateToCreateEmployee()">
-                            <span>Create</span>
-                        </button>
-                    ';
-                    echo '
-                        <button class="main" onclick="navigateToEmployeeTimesheet()">
-                            <span>Employee Timesheet</span>
-                        </button>
-                    ';
-                } elseif (isset($_SESSION["employeeuid"])) {
-                    echo '
-                        <button class="main" onclick="navigateToTimePunch()">
-                            <span>Time Punch</span>
-                         </button>
-                    ';
-                    echo '
-                        <button class="main" onclick="navigateToTimeSheet()">
-                            <span>Time Sheet</span>
-                         </button>
-                    ';
-                } else {
-                    header("location: ../index.php");
-                }
-            ?>
+        <p> User Profile:</p>
+        <p>Company: <?php echo $_SESSION['company']; ?></p>
+        <p><?php echo $_SESSION['company']; ?></p>
+        <div>
+            <button type="submit" name="redirect">View Time Tracking</button>
+        </div>
         
-            <button class="main" onclick="navigateToSchedule()">
-                <span>Schedule</span>
-            </button>
         
-            <button class="main" onclick="navigateToAnnouncement()">
-                <span>Announcements</span>
-              </button>
-
-            <button class="main" onclick="navigateToPayPeriod()">
-                <span>Pay Period</span>
-            </button>
-            
-            <button class="main" onclick="navigateToSettings()">
-                <span>Settings</span>
-            </button>
             
         
 
